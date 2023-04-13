b775dd4b8eda5e7a3e857095bff95b916c386f68
Lantunez05 <126211785+Lantunez05@users.noreply.github.com>
2023-04-13 01:51:41 -0600

    Update Main_07.c

--- Main_07.c
+++ Main_07.c
@@ -29,6 +29,7 @@
 #include <pic16f887.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include "pwm.h"
 
 #define _XTAL_FREQ 250000
 #define valPR2 194 // valor para 20Hz y prescaler de 16
@@ -36,75 +37,74 @@
 uint16_t i=0;
 
 void setup (void);
-void pwm_ini(void);
-void pwm_dc(uint16_t DutyCycle);
+//void pwm_ini(void);
+//void pwm_dc(uint16_t DutyCycle);
+
+void __interrupt() isr(void)
+{
+    if(ADIF)
+    {
+        if (ADCON0bits.CHS == 0b00000001)
+            CCPR1L = (ADRESH>>1)+124;
+            
+        else if (ADCON0bits.CHS == 0b00000010)
+            //CCPR2L = (ADRESH>>1)+124;
+             
+            ADIF=0;
+        
+    }
+    return;
+}
 
 void main() {
     setup();
-    pwm_ini();
+   PWM_Init();
     
     while (1)
     {
-     //   pwm_dc(50);
-        
+     ADCON0bits.CHS = 0b00000001; // Seleccion del canal AN1
+     __delay_ms(10);
+     ADCON0bits.GO =1;  // Habilita las conversiones de analogico a digital
+     __delay_ms(10);
+     while (ADCON0bits.GO_DONE); // Verificacion del canal AN1
+     CCPR1L = ADRESH;           // Mueve el valor almacenado en ADRESH a adc
+     __delay_ms(10);
+     
+     
+     ADCON0bits.CHS = 0b00000010;  // Cambio al canal AN2
+     __delay_ms(10);
+     ADCON0bits.GO = 1; // Iniciar la conversión en el canal AN2
+     __delay_ms(10);
+     while (ADCON0bits.GO_DONE); // Verificacion del canal AN2
+     CCPR2L = ADRESH;           // Mueve el valor almacenado en ADRESH a adc
+     __delay_ms(10);   
     }
     return;
 }
 
 void setup(void)
 {
-    OSCCON = 0x25;
-    ANSEL = 0x00;
-    ANSELH= 0x00;
-}
-
-void pwm_ini(void)
-{
-    /*
-     11.5.7 SETUP FOR PWM OPERATION
-        The following steps should be taken when configuring
-        the CCP module for PWM operation:
-        1. Disable the PWM pin (CCPx) output drivers as
-        an input by setting the associated TRIS bit.
-        2. Set the PWM period by loading the PR2 register.
-        3. Configure the CCP module for the PWM mode
-        by loading the CCPxCON register with the
-        appropriate values.
-        4. Set the PWM duty cycle by loading the CCPRxL
-        register and DCxB<1:0> bits of the CCPxCON
-        register.
-        5. Configure and start Timer2:
-        ? Clear the TMR2IF interrupt flag bit of the
-        PIR1 register.
-        ? Set the Timer2 prescale value by loading the
-        T2CKPS bits of the T2CON register.
-        ? Enable Timer2 by setting the TMR2ON bit of
-        the T2CON register.
-        6. Enable PWM output after a new PWM cycle has
-        started:
-        ? Wait until Timer2 overflows (TMR2IF bit of
-        the PIR1 register is set).
-        ? Enable the CCPx pin output driver by clearing
-        the associated TRIS bit.
-     
-     */
-    TRISC = 0X02; // Paso 1
-    PR2 = valPR2; // Paso 2 
-    CCP1CON = 0X1C; // Paso 3 y 4 config PWM, CCP1CON=xx0011xx
-    CCPR1L = 0x61; // Configuracion de ciclosd e trabajo
-    PIR1bits.TMR2IF=0; // Paso 5, limpiar bandera TMR2
-    T2CON =0X07;  // Post 1:1, T2_ON, PRE 16, T2CON=00000011
-    while (TMR2IF==0){}// Paso 6
-    PIR1bits.TMR2IF=0; // Esperar un ciclo del TMR2
-    TRISC = 0x00; // Salida PWM
     
-}
-
-void pwm_dc (uint16_t DutyCycle)// Valor entre 0 y 100
-{
-    CCPR1L = DutyCycle>>2; // Colocar los bits MSB de DutyCicle en CCPR1L
-    CCP1CON = CCP1CON&0xCF; // Colocar bit 5&6 = 0 para asignar valores
-    CCP1CON = CCP1CON|(0x30&(DutyCycle <<4));
+    TRISC = 0;
+    PORTC=0;
+    OSCCON = 0x25;
+    ANSEL = 0xFF;
+    ANSELH= 0x00;
+    
+    OSCCONbits.IRCF =0b0110; 
+    OSCCONbits.SCS = 1; 
+    //configuración de la interrupcion del TMR0
+    INTCONbits.T0IF = 0;
+    INTCONbits.T0IE = 0;
+    INTCONbits.GIE = 0;
+    
+    // Configuracion del ADC
+    ADCON0bits.ADCS = 0b01; // divisor de reloj de 32
+    __delay_ms(1);
+    ADCON1bits.ADFM = 0;    // Justificado a la izquierda 
+    ADCON1bits.VCFG0 = 0;
+    ADCON1bits.VCFG1 = 0;    // Referencia de voltaje 0
+    ADCON0bits.ADON = 1;   // habilitar el adc
+    ADIF =0;
     return;
-
 }
